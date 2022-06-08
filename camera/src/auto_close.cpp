d55b2d67660b765e8886532d55693b7a85c0050a
Mike Ellison <devellison@gmail.com>
2022-06-08 06:58:53 -0400

    Removed doc from regular builds, fixed platform issues on Win

--- camera/src/auto_close.cpp
+++ camera/src/auto_close.cpp
@@ -1,7 +1,17 @@
+#if _WIN32
+#define _CRT_DECLARE_NONSTDC_NAMES 1  // get unix-style read/write flags
+#define _CRT_NONSTDC_NO_DEPRECATE     // c'mon, I just want to call open().
+#define _CRT_SECURE_NO_WARNINGS
+#endif  // _WIN32
+
 #include "auto_close.hpp"
 
 #include <fcntl.h>
+#if __linux__
 #include <unistd.h>
+#elif _WIN32
+#include <io.h>
+#endif
 
 namespace zebral
 {
--- camera/test/camera_tests.cpp
+++ camera/test/camera_tests.cpp
@@ -2,7 +2,7 @@
 #define _CRT_DECLARE_NONSTDC_NAMES 1  // get unix-style read/write flags
 #define _CRT_NONSTDC_NO_DEPRECATE     // c'mon, I just want to call open().
 #define _CRT_SECURE_NO_WARNINGS
-#endif
+#endif  // _WIN32
 
 #include <fcntl.h>
 #include <atomic>
