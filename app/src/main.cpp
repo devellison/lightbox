4ba50c2c85c1fd9426bb9d9f4c8727e209993858
devellison <devellison@gmail.com>
2022-06-11 08:04:13 -0400

    Couple of features for saving images for testing stuff

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <filesystem>
 #include <iostream>
 #include "camera.hpp"
 #include "camera2cv.hpp"
@@ -17,6 +18,18 @@ void printHelp()
 {
   std::cout << "lightbox v" << LIGHTBOX_VERSION << " by Michael Ellison " << std::endl;
   std::cout << "Usage: lightbox CAMERA_INDEX SERIAL_PORT" << std::endl;
+}
+
+void saveImage(ZBA_TSTAMP time, const cv::Mat& img, const std::filesystem::path& path,
+               const std::string identifier = "")
+{
+  std::filesystem::path savepath = path;
+  std::string filename           = zba_format("image_{}", zba_local_time(time));
+  if (!identifier.empty()) filename += "_" + identifier;
+  filename += ".png";
+  savepath /= filename;
+
+  imwrite(savepath.string(), img);
 }
 
 int main(int argc, char* argv[])
@@ -64,28 +77,124 @@ int main(int argc, char* argv[])
     return to_int(Result::ZBA_CAMERA_ERROR);
   }
 
+  // Create image directory
+  constexpr static auto kImageDir = "images";
+  std::filesystem::path imageDir(kImageDir);
+  if (!std::filesystem::exists(imageDir))
+  {
+    std::filesystem::create_directory(imageDir);
+    if (!std::filesystem::exists(imageDir))
+    {
+      ZBA_ERR("WARNING: images folder does not exist!");
+      imageDir = std::filesystem::temp_directory_path();
+      ZBA_ERR("Saving images to {} instead.", imageDir.string());
+    }
+  }
+
   // Now open serial port
 
   // Start gui
   camera->Start();
+  bool buffering = false;
+  int pressed    = 0;
+  std::vector<cv::Mat> bufferMats;
+  cv::Mat accum;
+  const int kMaxBufferSize = 10;
 
-  int pressed = 0;
+  auto formatMaybe = camera->GetFormat();
+  ZBA_ASSERT(formatMaybe, "Must get camera format");
+  auto format = *formatMaybe;
+
+  cv::Rect roi(0, 0, format.width, format.height);
 
   do
   {
-    if (pressed == 'i')
-    {
-      std::cout << camera->GetCameraInfo() << std::endl;
-    }
     auto frame = camera->GetNewFrame();
+    cv::Mat img;
     if (frame)
     {
-      cv::Mat img = Converter::Camera2Cv(*frame);
-      cv::imshow("Camera", img);
+      img = Converter::Camera2Cv(*frame);
+      if (buffering)
+      {
+        if (accum.empty())
+        {
+          accum = cv::Mat::zeros(img.rows, img.cols, CV_64FC3);
+        }
+
+        cv::Mat adder;
+        img.convertTo(adder, CV_64FC3);
+        bufferMats.push_back(adder);
+        accum += adder;
+
+        while (bufferMats.size() > kMaxBufferSize)
+        {
+          accum -= (*bufferMats.begin());
+          bufferMats.erase(bufferMats.begin());
+        }
+
+        img = accum / static_cast<double>(bufferMats.size());
+        img.convertTo(img, CV_8UC3);
+      }
+      cv::imshow("Camera", img(roi));
     }
     else
     {
       ZBA_LOG("Got an empty frame in loop.");
+    }
+    switch (pressed)
+    {
+      case 'r':
+      {
+        // set ROI
+        if ((roi.width != img.cols) || (roi.height != img.rows))
+        {
+          roi = cv::Rect(0, 0, img.cols, img.rows);
+        }
+        else
+        {
+          cv::destroyWindow("Camera");
+          roi = cv::selectROI(img, true);
+          cv::destroyWindow("ROI selector");
+        }
+      }
+      break;
+      case 'i':
+      {
+        // camera info
+        std::cout << camera->GetCameraInfo() << std::endl;
+      }
+      break;
+      case 's':
+      {
+        // save
+        if (!img.empty())
+        {
+          saveImage(zba_now(), img(roi), imageDir);
+        }
+      }
+      break;
+      case 'x':
+      {
+        // Split channels
+        std::vector<cv::Mat> array;
+        cv::split(img(roi), array);
+        auto curTime = zba_now();
+        saveImage(curTime, array[0], imageDir, "blue");
+        saveImage(curTime, array[1], imageDir, "green");
+        saveImage(curTime, array[2], imageDir, "red");
+        cv::equalizeHist(array[0], array[0]);
+        cv::equalizeHist(array[1], array[1]);
+        cv::equalizeHist(array[2], array[2]);
+        saveImage(curTime, array[0], imageDir, "blue_eq");
+        saveImage(curTime, array[1], imageDir, "green_eq");
+        saveImage(curTime, array[2], imageDir, "red_eq");
+      }
+      case 'b':
+      {
+        // buffer the image 10x and accum to denoise
+        buffering ^= true;
+      }
+      break;
     }
   } while ('q' != (pressed = cv::waitKey(10)));
 
