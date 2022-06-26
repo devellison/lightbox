f4289663f33179a91de9f69b25bbf3a0fb272dba
devellison <devellison@gmail.com>
2022-06-26 11:48:00 -0400

    Moved python pindings under zebral folder

--- zebral/python/test/zebral_python_test.py
+++ zebral/python/test/zebral_python_test.py
@@ -5,11 +5,13 @@ import numpy
 import cv2
 import platform
 
+# This is the path we think our binding module is in.
+# This file is meant to run in automated unit tests.
 zebral_test_path = '../'
 zebral_format = 'YUYV'
 
 if (platform.system() == "Windows"):
-    zebral_test_path = '..\\Release\\'
+    zebral_test_path = '../Release/'
     zebral_format = 'YUY2'
 
 # Assumes is run in build/python/test with the module in build/python
@@ -18,9 +20,13 @@ sys.path.insert(0, zebral_test_path)
 
 import zebralpy
 
+retVal = 0
 camMgr = zebralpy.CameraManager()
 cameraList = camMgr.Enumerate()
-retVal = 0
+# We can support others, esp. with system decoding,
+# but it would make the reshape more complex.
+easyFormats = {'YUYV','NV12','YUY2'}
+
 for curCamera in cameraList:
     print(curCamera)
     camera = camMgr.Create(curCamera)
@@ -30,6 +36,11 @@ for curCamera in cameraList:
     format.width = 640
     format.height = 480
     format.fps = 30
+    
+    # Skip more complex formats for now in test.
+    if (format.format not in easyFormats):
+        continue
+    
     format.format = zebral_format
     camera.SetFormat(format, decodeType)
 
@@ -38,7 +49,8 @@ for curCamera in cameraList:
     if (frame):
         bgr = frame.data()
         bgr = bgr.reshape(frame.height(), frame.width(), frame.channels())
-        # Let's not in unit tests, but this shows the image is ok
+        # Let's no try popping up images in smoke tests, 
+        # but this shows the image is ok
         #cv2.imshow(curCamera.name,bgr)
         #cv2.waitKey(1000)
     else:
